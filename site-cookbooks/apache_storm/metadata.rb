7d984e9507f0a738ca63970985c371038bf940fc
Jonathan Reichhold <jreichhold@Jonathans-MacBook-Pro-2.local>
2014-04-11 13:18:32 -0700

    Fix metadata.rb deps

--- site-cookbooks/apache_storm/metadata.rb
+++ site-cookbooks/apache_storm/metadata.rb
@@ -5,3 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures apache_storm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          "java"
+depends          "runit"
+depends          "zeromq"
+supports         "ubuntu"
